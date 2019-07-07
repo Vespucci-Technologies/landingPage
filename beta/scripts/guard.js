53c2333168424b95189cb2c08c48aade9f055f78
Mansour DAWANT <mansour.dawant@gmail.com>
2019-07-08 00:12:05 +0200

    Fix UI

--- beta/scripts/guard.js
+++ beta/scripts/guard.js
@@ -2,10 +2,11 @@
  auth.onAuthStateChanged(user => {
   if (user) {
     console.log('user logged in: ', user);
-   
+    window.location = "https://vespuccianalytics.com/dashboard/";
+
   } else {
     console.log('user logged out');
-    window.location = "http://www.yoururl.com";
+    window.location = "https://vespuccianalytics.com/beta/";
 
 
   }
